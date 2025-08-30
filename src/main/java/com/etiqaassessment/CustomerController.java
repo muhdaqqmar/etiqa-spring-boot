30a8750bcdc3f5f4475d533c3bc194db5d623ad3
muhdaqqmar <127712023+muhdaqqmar@users.noreply.github.com>
2025-08-30 20:21:19 +0800

    create getMappingController for customer

---
+++ src/main/java/com/etiqaassessment/CustomerController.java
@@ -0,0 +1,35 @@
+package com.etiqaassessment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Locale;
+
+@RestController
+@RequestMapping("api/v1/customer")
+public class CustomerController {
+
+    @GetMapping
+    public List<Customers> getCustomers() {
+
+        return List.of(
+                new Customers(
+                        1,
+                        "Muhammad Aqqmar",
+                        "Zulkarnain",
+                        "muhdaqqmar@gmail.com",
+                        "0172979003"
+                ),
+                new Customers(
+                        2,
+                        "Aiman",
+                        "Sopie",
+                        "aiman@mail.com",
+                        "0191119191"
+                )
+        );
+    }
+
+}
